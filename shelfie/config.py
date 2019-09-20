0c3a0b02a79c65c577dc661538136b2b8e7c7a77
Vinay Keerthi <ktvkvinaykeerthi@gmail.com>
2019-09-20 13:50:31 +0530

    add config example, shelf topic update

--- shelfie/config.py
+++ shelfie/config.py
@@ -1,10 +1,59 @@
-"""Config reader"""
+"""Config reader
+Example config:
+
+{
+    "meta":{
+        "label": "a",
+        "is_visible": true
+    },
+    "mqtt": {
+        "host": "mosquitto",
+        "port": 1883,
+        "username": "shelfie",
+        "password": "secure-password",
+        "topics":
+            {
+                "shelf": "shelfie/{label}",
+                "alert": "shelfie/alert"
+            }
+    },
+    "lights": {
+        "hold_time": 10,
+        "blink_gap": 0.5,
+        "blink_times": 3,
+        "colors": {
+            "red": [255, 0, 0],
+            "blue": [0, 0, 255],
+            "green": [0, 255, 0],
+            "yellow": [255, 255, 0],
+            "orange": [255, 165, 0],
+            "pink": [255, 192, 203],
+            "cyan": [0, 255, 255],
+            "magenta": [255, 0, 255]
+        }
+    },
+    "networks": [
+        {
+            "ssid": "ssid1",
+            "password": "password1"
+        },
+        {
+            "ssid": "ssid2",
+            "password": "password2"
+        }
+       ]
+    }
+}
+
+"""
+
 import ujson as json
 
 with open("config.json", "r") as f:
     config = json.load(f)
 
-
 networks = config["networks"]
 mqtt = config["mqtt"]
 lights = config["lights"]
+meta = config["meta"]
+meta["topics"]["shelf"] = meta["topics"]["shelf"].format(label=meta["label"])
