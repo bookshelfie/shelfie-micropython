809d555b6e821b386b6d902f3418c59a3a8689f0
Vinay Keerthi <ktvkvinaykeerthi@gmail.com>
2019-09-20 15:08:58 +0530

    move listening step to main

--- shelfie/boot.py
+++ shelfie/boot.py
@@ -6,7 +6,6 @@ webrepl.start()
 import ujson as json
 import utils
 import light
-import shelfie
 
 if __name__ == __main__():
     with open("config.json", "r") as f:
@@ -14,4 +13,3 @@ if __name__ == __main__():
     utils.setup_network(config=config)
     light.clear(config=config)
     gc.collect()
-    shelfie.listen(config=config)
