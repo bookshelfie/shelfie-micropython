82943ed290f2088c2ba29774bdee20f5029ccc43
Vinay Keerthi <ktvkvinaykeerthi@gmail.com>
2019-09-20 14:56:22 +0530

    update listen, add conversion for message and topic datatypes, update locate.

--- shelfie/shelfie.py
+++ shelfie/shelfie.py
@@ -1,8 +1,10 @@
 """shelfie core library"""
 
-import light
-from umqtt.simple as MQTTClient
 import ujson as json
+import time
+from umqtt.simple as MQTTClient
+
+import light
 import config
 
 
@@ -14,28 +16,46 @@ def _type(topic):
     return False
 
 
-def alert(message):
+def alert(color, blink=True, times=2):
     """general alert"""
-    pass
 
 
-def locate(message):
+
+def locate(positions, color):
     """locates an item on the shelf"""
-    pass
+    if "." in positions:
+        positions, row = positions.split(".")
+    else:
+        row = 1
+    if ":" in positions:
+
+        start, end = positions.split(":")
+    else:
+        start = end = positions
+    start, end, row = int(start), int(end), int(row)
+    light.locate(start, end, row)
+    time.sleep(config.lights.hold_time)
+    light.clear()
 
 
 def process_message(topic, message):
     """Processes message"""
+    # TODO: convert the topic and message before using them.
+    topic = topic.decode()
+    message = json.loads(message.decode())
     if _type(topic) == "shelf":
-        locate(message)
+        locate(**message)
     elif _type(topic) == "alert":
-        alert(message)
+        alert(**message)
+    else:
+        alert(config.lights["colors"]["red"],)
 
 
 def listen():
     """Function that initiates listening to the mqtt topics."""
     client = MQTTCLient(
-        "shelf_nodemcu_{label}".format(label=config.meta["label"]),
+        "shelf_nodemcu_{label}".format(
+            label=config.meta["label"]),
         config.mqtt["host"],
         config.mqtt["port"],
         config.mqtt["username"],
@@ -45,3 +65,11 @@ def listen():
     client.connect()
     for topic in config.mqtt["topic"].values():
         client.subscribe(bytes("{}".format(topic), "utf-8"))
+
+    while True:
+        if True:
+            c.wait_msg() # blocking wait for message
+        else:
+            c.check_msg() # non-blocking wait for message
+            time.sleep(1)
+    client.disconnect()
