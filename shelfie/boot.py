4a5b90b42c0200f549e4d5cf6447511058f5817d
Vinay Keerthi <ktvkvinaykeerthi@gmail.com>
2019-09-20 12:32:33 +0530

    use light.py

--- shelfie/boot.py
+++ shelfie/boot.py
@@ -3,11 +3,15 @@
 import gc
 import webrepl
 webrepl.start()
+import ujson as json
 import utils
+import light
 import shelfie
 
 if __name__ == __main__():
-    utils.setup_network(config="config.json")
-    shelfie.clear_lights()
+    with open("config.json", "r") as f:
+        config = json.load(f)
+    utils.setup_network(config=config)
+    light.clear(config=config)
     gc.collect()
-    shelfie.listen(config="config.json")
+    shelfie.listen(config=config)
