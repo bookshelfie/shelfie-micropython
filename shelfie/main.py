596772e2bf12bad49bf3571167b6ed3da85f04c3
Vinay Keerthi <ktvkvinaykeerthi@gmail.com>
2019-09-28 10:27:15 +0530

    add main function with a restart for OSErrors

--- shelfie/main.py
+++ shelfie/main.py
@@ -1,6 +1,24 @@
 # main file.
 # This file is run after boot.py.
-import shelfie
-if __name__ == "__main__":
+
+
+def main(counter=0):
+    """main"""
+    import shelfie
     print("preparing to listen.")
-    shelfie.listen()
+    try:
+        shelfie.listen()
+    except OSError:
+        if counter <= 10:
+            main(counter+1)
+        else:
+            print("Rebooting system due to too many connection failures.")
+            import machine
+            machine.reset()
+    except KeyboardInterrupt:
+        raise
+    else:
+        print("Exitting")
+
+if __name__ == "__main__":
+    main()
