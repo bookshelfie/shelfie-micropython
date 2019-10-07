50606fc0fbc85dab7ca1bee4c03329f694e6d828
Vinay Keerthi <ktvkvinaykeerthi@gmail.com>
2019-10-07 19:15:30 +0530

    add parameter for config label

--- transfer_files.sh
+++ transfer_files.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ./webrepl/webrepl_cli.py -p $1 shelfie/config.py $2:/config.py
-./webrepl/webrepl_cli.py -p $1 config/config.json $2:/config.json
+./webrepl/webrepl_cli.py -p $1 config/config_$3.json $2:/config.json
 ./webrepl/webrepl_cli.py -p $1 shelfie/light.py $2:/light.py
 ./webrepl/webrepl_cli.py -p $1 shelfie/utils.py $2:/utils.py
 ./webrepl/webrepl_cli.py -p $1 shelfie/shelfie.py $2:/shelfie.py
